001adddd0af1fbf80efbcad4552f552b3720bafa
ngdanghau <ngdghau201@gmail.com>
2022-03-25 12:40:09 +0700

    Initial Commit

---
+++ MoneyPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoneyPro
+//
+//  Created by Hau Nguyen Dang on 25/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoneyPro/MoneyProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyProApp.swift
+//  MoneyPro
+//
+//  Created by Hau Nguyen Dang on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
